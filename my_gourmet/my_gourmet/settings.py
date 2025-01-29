57a94a749809573f464f10ba7b2a035022f84f95
Skylria <julianamaria.sv@gmail.com>
2025-01-28 22:03:39 -0300

    Add Login function

--- my_gourmet/my_gourmet/settings.py
+++ my_gourmet/my_gourmet/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mygourmetapp',
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'my_gourmet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- my_gourmet/mygourmetapp/models.py
+++ my_gourmet/mygourmetapp/models.py
@@ -2,3 +2,14 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    review = models.ForeignKey
+
+    def publish(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
